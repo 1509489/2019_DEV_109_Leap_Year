db7a52b69be29e28964735da28ff9b2742fd9126
Justice <popystone@gmail.com>
2019-02-20 15:41:00 +0000

    Implemented the logic for checking if a year is leap year or not and Tests should fail or pass depending on if the year is leap year or not

--- app/src/main/java/com/a2019_dev_109/leap_year/Presenter.kt
+++ app/src/main/java/com/a2019_dev_109/leap_year/Presenter.kt
@@ -3,6 +3,25 @@ package com.a2019_dev_109.leap_year
 class Presenter(private val view: Contract.View): Contract.Presenter {
 
     override fun isLeapYear(year: Int) {
+        if (divisibleBy400(year))
+            view.showIsLeapYearOrNot(true)
+        else if (divisibleBy100(year) && divisibleBy400(year))
+            view.showIsLeapYearOrNot(true)
+        else if (divisibleBy4(year) && !divisibleBy100(year))
+            view.showIsLeapYearOrNot(true)
+        else
+            view.showIsLeapYearOrNot(false)
+    }
 
+    private fun divisibleBy400(year: Int):Boolean{
+        return year % 400 == 0
+    }
+
+    private fun divisibleBy100(year: Int):Boolean{
+        return year % 100 == 0
+    }
+
+    private fun divisibleBy4(year: Int):Boolean{
+        return year % 4 == 0
     }
 }
